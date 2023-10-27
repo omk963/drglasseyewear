1c5494fd50de05d55f0005a472ac052837b0d4a4
omk963 <132976939+omk963@users.noreply.github.com>
2023-10-27 12:46:26 -0700

    index page
    adjusted css and js portion of index.html

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-var prevScrollpos = window.scrollY;
-window.onscroll = function() {
-    var currentScrollPos = window.scrollY;
-    if(prevScrollpos > currentScrollPos) {
-        document.getElementsByClassName("top").style.top = '0';
-    } else {
-        document.getElementsByClassName("top").style.top = '-70px';
-    }
-    prevScrollpos = currentScrollPos;
+var bgImages = ["media/carin layout.png","media/display of sunglasses on a table.png","media/dr glass posters.png", "media/left part of store display.png", "media/sunglasses display.png", "media/three glasses in front of bear figure that has yellow heart shaped sunglasses on it.png", "media/three sunglasses with plants behind.png"]
+var counter = 0;
+function bgSlideshow() {
+  document.getElementById('slideshow').style.backgroundImage = 'url(' + bgImages[counter] + ')';
+  if(counter < bgImages.length - 1) {
+    counter++
+  } else {
+    counter = 0;
+  }
+ setTimeout(bgSlideshow, 8000)
 }
 
+window.onload = bgSlideshow();
 
-var quotes = ['hi', 'bye', 'lol', 'hello', 'world']
-function randomQuotes() {
-    var idx = Math.floor(Math.random() * quotes.length);
-    document.getElementById('quote').innerHTML = quotes[idx];}
+var quotes = [
+  "The eyes are the window to the soul. Our frames? The perfect frame for that window.",
+  "Vision is the art of seeing what is invisible to others. - Jonathan Swift",
+  "The world is a beautiful place. Make sure you see it clearly.",
+  "Your style is your signature. Let our opticals be your fashion statement.",
+  "Clear vision is not just a luxury; it's a necessity. We're here to make it stylish.",
+  "Style is a reflection of your attitude and your personality. Our opticals are the perfect mirror.",
+  "Elegance is the only beauty that never fades. Our opticals are timeless.",
+  "Vision is the art of seeing what's possible. We help you see it all.",
+  "Clarity in vision. Clarity in life.",
+  "The world is a kaleidoscope of color and beauty. Our opticals enhance your view.",
+  "Seeing is not enough; you have to feel what you see. Feel the difference with us.",
+  "Frames that frame your world. We bring your vision into focus.",
+  "Eyes wide open, style unlimited. Discover a new perspective with our opticals.",
+  "Your eyes are precious. Your eyewear should be too.",
+  "Eyes are the storytellers of our lives. Let your eyewear tell your unique story.",
+  "See the world clearly, beautifully, and your way.",
+  "Enhance your vision, enrich your life. Explore the world in style."
+];
+window.onload = function randomQuotes() {
+  var idx = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quote").innerHTML = quotes[idx];
+};
